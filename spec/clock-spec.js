6f85fc9822fd986a070578e0b7769a26503c6e5f
Ian Goodrich <iangoodrich@hotmail.com>
2018-03-16 15:14:30 -0700

    add close diff method. Still off a bit.

--- spec/clock-spec.js
+++ spec/clock-spec.js
@@ -1,22 +1,30 @@
 import {SpaceClock} from './../src/js/clock.js';
 
 describe('SpaceClock', function(){
+    let bday;
+    let now;
+  beforeEach(function() {
+     bday = new SpaceClock(1976, 12, 17);
+     now = new SpaceClock(2018, 3, 17);
+   });
 
-  it('should take a number and return a rounded year', function(){
-    let bday = new SpaceClock(1976, 12, 17);
-    let outcome = bday.roughYears();
+
+  it('should take a number and return a rounded year from now', function(){
+    // let bday = new SpaceClock(1976, 12, 17);
+    let outcome = bday.roughYearsFromNow();
     expect(outcome).toEqual(41);
   })
 
   it('should create a new JS date object from the SpaceClock object', function(){
-    let bday = new SpaceClock(1976, 12, 17);
+    // let bday = new SpaceClock(1976, 12, 17);
     let outcome = bday.makeDate();
     let result = outcome.toString();
   expect(result).toEqual("Fri Dec 17 1976 00:00:00 GMT-0800 (Pacific Standard Time)");
   })
 
-  it('should take two dates and return the difecence in seconds', function(){
-    ///
+  it('should take two dates and return the difecence in rounded years', function(){
+    let result = bday.roughYearsDiff(now);
+    expect(result).toEqual(41);
   })
 
   it('should take a date from the past and return the difference in extra terrestrial years.', function(){
--- src/js/clock.js
+++ src/js/clock.js
@@ -11,14 +11,37 @@ export class SpaceClock{
 
   makeDate(){
     let newDate = new Date(this.year, this.month, this.day, this.minuet, this.second, this.mili)
-
     return newDate;
   }
 
-  roughYears(){
-    let now = new Date();
-    let rYears = Math.floor((now - this.makeDate())/31536000000);
+  roughYearsFromNow(){
+    let nowish = new Date();
+    let rYears = Math.floor((nowish - this.makeDate())/31536000000);
     return rYears;
   }
+
+  roughYearsDiff(clock2){
+    let earlier = this.makeDate();
+    let later = clock2.makeDate();
+    let milis  = later - earlier;
+    let roughDiff = Math.floor(milis/31536000000);
+    return roughDiff;
+  }
+
+  closeDiff(clock2){
+  let a = this.makeDate();
+  let b = clock2.makeDate();
+  let coarseSecs = (a - b)/1000;
+  let years = Math.floor(coarseSecs/31536000);
+  let weeks = Math.floor((coarseSecs % 31536000) / 640800);
+  let days = Math.floor(((coarseSecs % 31536000) % 640800) / 86400);
+  let hours = Math.floor((((coarseSecs % 31536000) % 640800) % 86400) / 3600);
+  let mins = Math.floor(((((coarseSecs % 31536000) % 640800) % 86400) % 3600)/60);
+  let secs = Math.floor(((((coarseSecs % 31536000) % 640800) % 86400) % 3600)%60);
+  let answer = `years ${years}, weeks ${weeks}, days ${days}, hours ${hours} minuets ${mins} seconds ${secs}`;
+
+  return answer;
+}
+
 
 }
