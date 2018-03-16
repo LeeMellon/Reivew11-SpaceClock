30cfb10f8551bd77bf2eb201b7932c9d122fab8c
Ian Goodrich <iangoodrich@hotmail.com>
2018-03-16 12:24:11 -0700

    add SpaceClock class

--- src/js/clock.js
+++ src/js/clock.js
@@ -1,7 +1,15 @@
-// export class SpaceClock{
-//   constructor(){
-//     this.this= that;
-//   }
-//
-//
-// }
+export class SpaceClock{
+  constructor(year, month, day, hour =0, minuet =0, second =0, mili =0){
+    this.year= year;
+    this.month = month-1;
+    this.day = day;
+    this.hour = hour;
+    this.minuet = minuet;
+    this.second = second;
+    this.mili = mili;
+  }
+
+
+
+
+}
